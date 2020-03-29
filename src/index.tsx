2c6195014e7382fe07edd3c6f753ce6a683f90f0
Efraim Andrade <efraim.dev@gmail.com>
2020-03-28 22:24:15 -0300

    a

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ export default function App() {
       <StatusBar />
 
       <Container>
-        <Text>boilerplate ?</Text>
+        <Text>boilerplate ???</Text>
       </Container>
     </>
   );
